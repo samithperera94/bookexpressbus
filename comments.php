06f6d7fcd8afa11e16026a00e75834ea4cabfbfb
Buddhika Shaminda Rathnayaka <rmbsrathnayaka@gmail.com>
2018-04-29 21:43:11 +0530

    feedback
    without ci

---
+++ comments.php
@@ -0,0 +1,26 @@
+<?php 
+
+$connection = mysqli_connect('127.0.0.1','root','','bookexpressbus');
+
+if(mysqli_connect_errno()){
+	die("Somethig Ent wrong");
+}else{
+	echo 'successfully connected';
+}
+
+$name=$_POST['name'];
+$feedback=$_POST['feedback'];
+ 
+ //echo $name." ".$feedback;
+$query="INSERT INTO feedback (name,feedback) VALUES('{$name}','{$feedback}') ";
+
+$result = mysqli_query($connection,$query);
+
+// if($result){
+// 	echo 'success';
+// }else{
+// 	echo 'fail';
+//}
+ include "compaling.php" ;
+
+ ?>
